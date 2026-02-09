a139ba71050134edb0599ec928c484c69a340c6e
abhyudaymishr <abhymishrgkp@gmail.com>
2026-02-10 02:09:52 +0530

    Expose structured public API

--- src/knot_recognition/__init__.py
+++ src/knot_recognition/__init__.py
@@ -1,5 +1,30 @@
 __version__ = "0.1.0"
 
+from .dataset import DatasetConfig, KnotImageDataset
+from .gauss_pd import GaussPDConfig, GaussPDExtractor, extract_gauss_code
+from .infer import InferenceConfig, KnotRecognizer, infer_image, load_checkpoint
+from .model import ResNetConfig, build_resnet, get_resnet
+from .preprocess import PreprocessConfig, Preprocessor, preprocess_for_skeleton
+from .train import TrainConfig, Trainer, train
 
-from .model import get_resnet
-from .dataset import KnotImageDataset
+__all__ = [
+    "__version__",
+    "DatasetConfig",
+    "KnotImageDataset",
+    "GaussPDConfig",
+    "GaussPDExtractor",
+    "extract_gauss_code",
+    "InferenceConfig",
+    "KnotRecognizer",
+    "infer_image",
+    "load_checkpoint",
+    "ResNetConfig",
+    "build_resnet",
+    "get_resnet",
+    "PreprocessConfig",
+    "Preprocessor",
+    "preprocess_for_skeleton",
+    "TrainConfig",
+    "Trainer",
+    "train",
+]
